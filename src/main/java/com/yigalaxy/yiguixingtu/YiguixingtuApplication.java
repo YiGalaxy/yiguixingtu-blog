9f1f7e94b179db503525499d428cc8ea35eb992d
别太在亿啦 <2175548220@qq.com>
2026-09-09 06:37:03 +0800

    构建了项目使用的docker环境（MySQL8,Redis7）

---
+++ src/main/java/com/yigalaxy/yiguixingtu/YiguixingtuApplication.java
@@ -0,0 +1,13 @@
+package com.yigalaxy.yiguixingtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiguixingtuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiguixingtuApplication.class, args);
+    }
+
+}

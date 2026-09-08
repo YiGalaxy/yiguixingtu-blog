ddd2e0955fe4c31f29f4df7f9a163b79db70275c
别太在亿啦 <2175548220@qq.com>
2026-09-09 07:40:06 +0800

    从JPA换用到Mybatis-Puls，进行了分页插件的配置

---
+++ src/main/java/com/yigalaxy/yiguixingtu/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.yigalaxy.yiguixingtu.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.yigalaxy.yiguixingtu.**.mapper")
+/**
+ * 配置MyBatis-Plus
+ */
+public class MybatisPlusConfig {
+
+    //初始化分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
